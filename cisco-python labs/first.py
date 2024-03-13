0b8b56263eb143ba772ed7a1e258ec8babef9f07
Jayanth535 <jayanthsaitrinadh@gmail.com>
2024-03-13 06:58:30 +0530

    modify changes

--- cisco-python labs/first.py
+++ cisco-python labs/first.py
@@ -1,2 +1,13 @@
 n=int(input("Enter the number which you want: "))
 print(n>=100)
+
+
+# output
+# Sample input: 99
+#  Expected output: False
+# Output
+# Sample input: 100
+# Expected output:True
+# Output
+# Sample input:101
+# Expected output:True

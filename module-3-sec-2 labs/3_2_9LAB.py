a8aa2372b1472065afc0b0d5a6b6f67916d4cb7b
Jayanth535 <jayanthsaitrinadh@gmail.com>
2024-03-14 16:01:24 +0530

    3-2-9 lab committed

---
+++ module-3-sec-2 labs/3_2_9LAB.py
@@ -0,0 +1,11 @@
+
+while True:
+    name=input("Enter the name: ")
+    if name == "chupacabra":
+        break
+print("You've successfully left the loop.")    
+
+# output:
+#     Enter the name: jaynth
+# Enter the name: chupacabra
+# You've successfully left the loop.
